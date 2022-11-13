2c5b9bb83f31289e7bca2a3a4cc396f310ec47af
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-13 12:55:02 -0500

    sketch haiku editor

--- README.md
+++ README.md
@@ -1,46 +1,30 @@
-# Getting Started with Create React App
+# Twenty Second Haiku
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Compose a haiku from random words in twenty seconds.
 
-## Available Scripts
+## What is a haiku?
 
-In the project directory, you can run:
+A haiku is a poem with three lines.
+The first and last lines of a haiku each have five syllables.
+The middle line has seven syllables.
 
-### `npm start`
+```text
+This is a haiku
+That I wrote to help teach you
+How to play my game
+```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Above is an example of a haiku.
+Note the number of _syllables_ (not words) on each line of the poem.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## How do you play _Twenty Second Haiku_?
 
-### `npm test`
+Twenty Second Haiku is a browser-based game. You can [play it here]().
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+The goal of _Twenty Second Haiku_ is to compose a haiku from random words in twenty seconds.
 
-### `npm run build`
+When you begin a game of _Twenty Second Haiku_, you will be presented with a **collection of random words**, an **empty haiku template**, and a **twenty second timer**.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### How do you win?
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+You win if you create a valid haiku from the provided words before the timer expires.
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  CheckCircle,
+  InfoOutlined,
+  Refresh,
+  ReportProblem,
+  Timer,
+} from "@mui/icons-material";
+import {
+  Button,
+  Chip,
+  Container,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import randomWords from "random-words";
+import { useEffect, useMemo, useState } from "react";
+import { syllable } from "syllable";
 
-function App() {
+export default () => {
+  const [dictionary, setDictionary] = useState<string[]>([]);
+
+  const [firstLine, setFirstLine] = useState<string>("");
+
+  const [secondLine, setSecondLine] = useState<string>("");
+
+  const [thirdLine, setThirdLine] = useState<string>("");
+
+  const firstLineSyllables = useMemo(() => {
+    return syllable(firstLine);
+  }, [firstLine]);
+
+  const isFirstLineFiveSyllables = useMemo(() => {
+    return firstLineSyllables === 5;
+  }, [firstLineSyllables]);
+
+  const secondLineSyllables = useMemo(() => {
+    return syllable(secondLine);
+  }, [secondLine]);
+
+  const isSecondLineSevenSyllables = useMemo(() => {
+    return secondLineSyllables === 7;
+  }, [secondLineSyllables]);
+
+  const thirdLineSyllables = useMemo(() => {
+    return syllable(thirdLine);
+  }, [thirdLine]);
+
+  const isThirdLineFiveSyllables = useMemo(() => {
+    return thirdLineSyllables === 5;
+  }, [thirdLineSyllables]);
+
+  useEffect(() => {
+    setDictionary(randomWords(17));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container maxWidth={"sm"}>
+      <Stack spacing={5}>
+        <Paper sx={{ padding: 0 }} elevation={0}>
+          <Stack spacing={2}>
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+            >
+              <Stack direction={"row"} alignItems={"center"} spacing={1}>
+                <Typography variant={"h6"}>Twenty Second Haiku</Typography>
+                <IconButton aria-label="info">
+                  <InfoOutlined color={"primary"} />
+                </IconButton>
+              </Stack>
 
-export default App;
+              <Stack direction={"row"} alignItems={"center"} spacing={1}>
+                <IconButton aria-label="refresh">
+                  <Refresh />
+                </IconButton>
+                <Chip icon={<Timer />} label="00:00" color={"error"} />
+              </Stack>
+            </Stack>
+            <Paper variant={"outlined"} sx={{ padding: 2 }}>
+              <Grid container spacing={2}>
+                {dictionary.map((word) => {
+                  const poem = firstLine + " " + secondLine + " " + thirdLine;
+                  const isWordUsed = poem
+                    .toLowerCase()
+                    .split(" ")
+                    .includes(word);
+                  return (
+                    <Grid item xs={"auto"}>
+                      <Chip
+                        label={word}
+                        variant={isWordUsed ? undefined : "outlined"}
+                        color={"success"}
+                      ></Chip>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Paper>
+            <Stack spacing={1}>
+              <TextField
+                autoFocus={true}
+                value={firstLine}
+                onChange={(event) => {
+                  setFirstLine(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position={"end"}>
+                      {firstLineSyllables}/5
+                    </InputAdornment>
+                  ),
+                  startAdornment: (
+                    <InputAdornment position={"start"}>
+                      {isFirstLineFiveSyllables ? (
+                        <CheckCircle color={"success"} />
+                      ) : (
+                        <ReportProblem color={"warning"}></ReportProblem>
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+              <TextField
+                value={secondLine}
+                onChange={(event) => {
+                  setSecondLine(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position={"end"}>
+                      {secondLineSyllables}/7
+                    </InputAdornment>
+                  ),
+                  startAdornment: (
+                    <InputAdornment position={"start"}>
+                      {isSecondLineSevenSyllables ? (
+                        <CheckCircle color={"success"} />
+                      ) : (
+                        <ReportProblem color={"warning"}></ReportProblem>
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+              <TextField
+                value={thirdLine}
+                onChange={(event) => {
+                  setThirdLine(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position={"end"}>
+                      {thirdLineSyllables}/5
+                    </InputAdornment>
+                  ),
+                  startAdornment: (
+                    <InputAdornment position={"start"}>
+                      {isThirdLineFiveSyllables ? (
+                        <CheckCircle color={"success"} />
+                      ) : (
+                        <ReportProblem color={"warning"}></ReportProblem>
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+            </Stack>
+            <Stack>
+              <Stack
+                direction={"row"}
+                spacing={1}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+              >
+                <Button
+                  fullWidth={true}
+                  size={"large"}
+                  variant={"contained"}
+                  disabled={
+                    !(
+                      isFirstLineFiveSyllables &&
+                      isSecondLineSevenSyllables &&
+                      isThirdLineFiveSyllables
+                    )
+                  }
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Paper>
+      </Stack>
+    </Container>
+  );
+};
