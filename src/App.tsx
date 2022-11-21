388730b83be85af00910761a5e329c3cb62901e4
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-21 17:36:08 -0500

    reorganize layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import {
   CheckCircle,
+  GitHub,
   PlayArrow,
   Refresh,
   ReportProblem,
-  Timer,
+  SportsEsports,
 } from "@mui/icons-material";
 import {
   Button,
   Chip,
   Container,
+  Fab,
   Grid,
   IconButton,
   InputAdornment,
@@ -88,210 +90,222 @@ export const App = () => {
   }, [dictionary.length, timerInterval]);
 
   return (
-    <Container maxWidth={"sm"}>
-      <Stack spacing={5}>
-        <Paper sx={{ padding: 0 }} elevation={0}>
-          <Stack spacing={2}>
-            <Stack
-              direction={"row"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-            >
-              <Stack direction={"row"} alignItems={"center"} spacing={1}>
-                <Typography variant={"h6"}>Twenty Second Haiku</Typography>
-              </Stack>
-
-              <Stack direction={"row"} alignItems={"center"} spacing={1}>
-                {dictionary.length > 0 ? (
-                  <IconButton
-                    ref={refreshButtonRef}
-                    aria-label={"refresh"}
-                    onClick={() => {
-                      setFirstLine("");
-                      setSecondLine("");
-                      setThirdLine("");
-                      setDictionary(randomWords(17));
-                      setRemainingMilliseconds(timerDurationMilliseconds);
-                    }}
-                  >
-                    <Refresh color={"primary"} />
-                  </IconButton>
-                ) : (
-                  <IconButton
-                    ref={playButtonRef}
-                    aria-label={"play"}
-                    onClick={() => {
-                      setDictionary(randomWords(17));
-                    }}
-                  >
-                    <PlayArrow color={"primary"} />
-                  </IconButton>
-                )}
-                <Chip
-                  icon={<Timer />}
-                  label={`00:${Math.floor(remainingMilliseconds / 1000)
-                    .toString()
-                    .padStart(2, "0")}:${(remainingMilliseconds % 1000)
-                    .toString()
-                    .padStart(3, "0")}`}
-                  color={
-                    remainingMilliseconds < timerDurationMilliseconds / 4
-                      ? "error"
-                      : remainingMilliseconds < timerDurationMilliseconds / 2
-                      ? "warning"
-                      : "primary"
-                  }
-                  variant={
-                    remainingMilliseconds < timerDurationMilliseconds / 2
-                      ? undefined
-                      : "outlined"
-                  }
-                />
-              </Stack>
-            </Stack>
-            <Paper variant={"outlined"} sx={{ padding: 2 }}>
-              {dictionary.length > 0 ? (
-                <Grid container spacing={2}>
-                  {dictionary.map((word, index) => {
-                    const poem = firstLine + " " + secondLine + " " + thirdLine;
-                    const isWordUsed = poem
-                      .toLowerCase()
-                      .split(" ")
-                      .includes(word);
-                    return (
-                      <Grid item xs={"auto"} key={index}>
-                        <Chip
-                          label={word}
-                          variant={isWordUsed ? undefined : "outlined"}
-                          color={"success"}
-                        ></Chip>
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              ) : (
-                <Typography>
-                  Compose a haiku from random words in twenty seconds.
-                </Typography>
-              )}
-            </Paper>
-            <Stack spacing={1}>
-              <TextField
-                onClick={() => {
-                  if (dictionary.length < 1 && playButtonRef.current) {
-                    playButtonRef.current.focus();
-                  }
-
-                  if (remainingMilliseconds < 1 && refreshButtonRef.current) {
-                    refreshButtonRef.current.focus();
-                  }
-                }}
-                inputRef={firstLineRef}
-                disabled={dictionary.length < 1 || remainingMilliseconds < 1}
-                value={firstLine}
-                onChange={(event) => {
-                  setFirstLine(event.target.value);
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position={"end"}>
-                      {firstLineSyllables}/5
-                    </InputAdornment>
-                  ),
-                  startAdornment: (
-                    <InputAdornment position={"start"}>
-                      {isFirstLineFiveSyllables ? (
-                        <CheckCircle color={"success"} />
-                      ) : (
-                        <ReportProblem color={"warning"}></ReportProblem>
-                      )}
-                    </InputAdornment>
-                  ),
-                }}
-              ></TextField>
-              <TextField
-                onClick={() => {
-                  if (dictionary.length < 1 && playButtonRef.current) {
-                    playButtonRef.current.focus();
-                  }
-
-                  if (remainingMilliseconds < 1 && refreshButtonRef.current) {
-                    refreshButtonRef.current.focus();
-                  }
-                }}
-                disabled={dictionary.length < 1 || remainingMilliseconds < 1}
-                value={secondLine}
-                onChange={(event) => {
-                  setSecondLine(event.target.value);
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position={"end"}>
-                      {secondLineSyllables}/7
-                    </InputAdornment>
-                  ),
-                  startAdornment: (
-                    <InputAdornment position={"start"}>
-                      {isSecondLineSevenSyllables ? (
-                        <CheckCircle color={"success"} />
-                      ) : (
-                        <ReportProblem color={"warning"}></ReportProblem>
-                      )}
-                    </InputAdornment>
-                  ),
-                }}
-              ></TextField>
-              <TextField
-                onClick={() => {
-                  if (dictionary.length < 1 && playButtonRef.current) {
-                    playButtonRef.current.focus();
-                  }
-
-                  if (remainingMilliseconds < 1 && refreshButtonRef.current) {
-                    refreshButtonRef.current.focus();
-                  }
-                }}
-                disabled={dictionary.length < 1 || remainingMilliseconds < 1}
-                value={thirdLine}
-                onChange={(event) => {
-                  setThirdLine(event.target.value);
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position={"end"}>
-                      {thirdLineSyllables}/5
-                    </InputAdornment>
-                  ),
-                  startAdornment: (
-                    <InputAdornment position={"start"}>
-                      {isThirdLineFiveSyllables ? (
-                        <CheckCircle color={"success"} />
-                      ) : (
-                        <ReportProblem color={"warning"}></ReportProblem>
-                      )}
-                    </InputAdornment>
-                  ),
-                }}
-              ></TextField>
-            </Stack>
-            <Button
-              fullWidth={true}
-              size={"large"}
-              variant={"contained"}
-              disabled={
-                !(
-                  remainingMilliseconds > 0 &&
-                  isFirstLineFiveSyllables &&
-                  isSecondLineSevenSyllables &&
-                  isThirdLineFiveSyllables
-                )
+    <Container maxWidth={"sm"} style={{ padding: "1rem" }}>
+      <Paper variant={"outlined"}>
+        <Stack spacing={2} padding={3}>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            spacing={2}
+          >
+            <Chip
+              style={{ fontSize: "2rem", padding: "2rem" }}
+              label={`${Math.floor(remainingMilliseconds / 1000)
+                .toString()
+                .padStart(2, "0")}:${(remainingMilliseconds % 1000)
+                .toString()
+                .padStart(3, "0")}`}
+              color={
+                remainingMilliseconds < timerDurationMilliseconds / 4
+                  ? "error"
+                  : remainingMilliseconds < timerDurationMilliseconds / 2
+                  ? "warning"
+                  : "info"
               }
-            >
-              Submit
-            </Button>
+            />
+            {dictionary.length > 0 ? (
+              <Fab
+                size={"large"}
+                color={"primary"}
+                ref={refreshButtonRef}
+                aria-label={"refresh"}
+                onClick={() => {
+                  setFirstLine("");
+                  setSecondLine("");
+                  setThirdLine("");
+                  setDictionary(randomWords(10));
+                  setRemainingMilliseconds(timerDurationMilliseconds);
+                }}
+              >
+                <Refresh />
+              </Fab>
+            ) : (
+              <Fab
+                size={"large"}
+                color={"primary"}
+                ref={playButtonRef}
+                aria-label={"play"}
+                onClick={() => {
+                  setDictionary(randomWords(10));
+                }}
+              >
+                <PlayArrow />
+              </Fab>
+            )}
           </Stack>
-        </Paper>
-      </Stack>
+          <Paper sx={{ padding: 4 }} elevation={0}>
+            {dictionary.length > 0 ? (
+              <Grid container spacing={2}>
+                {dictionary.map((word, index) => {
+                  const poem = firstLine + " " + secondLine + " " + thirdLine;
+                  const isWordUsed = poem
+                    .toLowerCase()
+                    .split(" ")
+                    .includes(word);
+                  return (
+                    <Grid item xs={"auto"} key={index}>
+                      <Chip
+                        sx={{ fontSize: "1rem" }}
+                        label={word}
+                        variant={isWordUsed ? undefined : "outlined"}
+                        color={"success"}
+                      ></Chip>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            ) : (
+              <Typography>
+                Compose a haiku from random words in twenty seconds.
+              </Typography>
+            )}
+          </Paper>
+          <Stack spacing={1}>
+            <TextField
+              onClick={() => {
+                if (dictionary.length < 1 && playButtonRef.current) {
+                  playButtonRef.current.focus();
+                }
+
+                if (remainingMilliseconds < 1 && refreshButtonRef.current) {
+                  refreshButtonRef.current.focus();
+                }
+              }}
+              inputRef={firstLineRef}
+              disabled={dictionary.length < 1 || remainingMilliseconds < 1}
+              value={firstLine}
+              onChange={(event) => {
+                setFirstLine(event.target.value);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position={"end"}>
+                    {firstLineSyllables}/5
+                  </InputAdornment>
+                ),
+                startAdornment: (
+                  <InputAdornment position={"start"}>
+                    {isFirstLineFiveSyllables ? (
+                      <CheckCircle color={"success"} />
+                    ) : (
+                      <ReportProblem color={"warning"}></ReportProblem>
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <TextField
+              onClick={() => {
+                if (dictionary.length < 1 && playButtonRef.current) {
+                  playButtonRef.current.focus();
+                }
+
+                if (remainingMilliseconds < 1 && refreshButtonRef.current) {
+                  refreshButtonRef.current.focus();
+                }
+              }}
+              disabled={dictionary.length < 1 || remainingMilliseconds < 1}
+              value={secondLine}
+              onChange={(event) => {
+                setSecondLine(event.target.value);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position={"end"}>
+                    {secondLineSyllables}/7
+                  </InputAdornment>
+                ),
+                startAdornment: (
+                  <InputAdornment position={"start"}>
+                    {isSecondLineSevenSyllables ? (
+                      <CheckCircle color={"success"} />
+                    ) : (
+                      <ReportProblem color={"warning"}></ReportProblem>
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <TextField
+              onClick={() => {
+                if (dictionary.length < 1 && playButtonRef.current) {
+                  playButtonRef.current.focus();
+                }
+
+                if (remainingMilliseconds < 1 && refreshButtonRef.current) {
+                  refreshButtonRef.current.focus();
+                }
+              }}
+              disabled={dictionary.length < 1 || remainingMilliseconds < 1}
+              value={thirdLine}
+              onChange={(event) => {
+                setThirdLine(event.target.value);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position={"end"}>
+                    {thirdLineSyllables}/5
+                  </InputAdornment>
+                ),
+                startAdornment: (
+                  <InputAdornment position={"start"}>
+                    {isThirdLineFiveSyllables ? (
+                      <CheckCircle color={"success"} />
+                    ) : (
+                      <ReportProblem color={"warning"}></ReportProblem>
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+          </Stack>
+          <Button
+            fullWidth={true}
+            size={"large"}
+            variant={"contained"}
+            disabled={
+              !(
+                remainingMilliseconds > 0 &&
+                isFirstLineFiveSyllables &&
+                isSecondLineSevenSyllables &&
+                isThirdLineFiveSyllables
+              )
+            }
+          >
+            Submit
+          </Button>
+          <Stack
+            direction={"row"}
+            spacing={1}
+            justifyContent={"center"}
+            padding={1}
+          >
+            <IconButton
+              href={"https://github.com/benrosen/twenty-second-haiku"}
+              target={"_blank"}
+            >
+              <GitHub />
+            </IconButton>
+            <IconButton
+              href={"https://brosen.itch.io/twenty-second-haiku"}
+              target={"_blank"}
+            >
+              <SportsEsports />
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Paper>
     </Container>
   );
 };
