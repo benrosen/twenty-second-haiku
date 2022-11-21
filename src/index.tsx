1ae46270a48d03c1cb472b456b9bfadadb67c4d1
Ben Rosen <benjamin.p.rosen@gmail.com>
2022-11-21 17:36:42 -0500

    configure dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
@@ -11,9 +13,18 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
